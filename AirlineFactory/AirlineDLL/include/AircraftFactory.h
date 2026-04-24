03c00e54d6b873acc2b59e54be79cf8610dcb8ab
Wangzhiyuan <344182022@qq.com>
2026-04-25 00:11:34 +0700

    Update AircraftFactory.h

--- AirlineFactory/AirlineDLL/include/AircraftFactory.h
+++ AirlineFactory/AirlineDLL/include/AircraftFactory.h
@@ -1,10 +1,9 @@
 #pragma once
-
 #include "AbstractAircraft.h"
 #include "dll_export.h"
 
 class AIRLINE_API AircraftFactory {
 public:
-    virtual ~AircraftFactory() = default;
-    virtual AbstractAircraft* Create() = 0;
+    virtual ~AircraftFactory() = default; // Виртуальный деструктор, обеспечивает корректное освобождение памяти подклассами
+    virtual AbstractAircraft* Create() = 0; // Чисто виртуальный метод: создаёт объект самолёта, обязательно реализуется в подклассах
 };
