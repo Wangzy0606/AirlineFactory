70dde70fb2fc410293a7e387a367115659f3ccfa
Wangzhiyuan <344182022@qq.com>
2026-04-25 00:19:33 +0700

    Update PassengerJetFactory.cpp

--- AirlineFactory/AirlineDLL/src/PassengerJetFactory.cpp
+++ AirlineFactory/AirlineDLL/src/PassengerJetFactory.cpp
@@ -4,6 +4,7 @@ PassengerJetFactory::PassengerJetFactory(std::string model, double speed, double
     : _model(model), _speed(speed), _alt(alt), _pass(pass), _range(range) {
 }
 
+// Реализация фабричного метода: создаёт конкретный пассажирский самолёт и возвращает его
 AbstractAircraft* PassengerJetFactory::Create() {
     return new PassengerJet(_model, _speed, _alt, _pass, _range);
 }
